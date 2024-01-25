df38ccce63ae4f6be569c5fb67680cf2e1c561d1
Manuel <arredondo23manuel@gmail.com>
2024-01-25 13:33:01 -0600

    Model

---
+++ Test/MAUI/Models/Videogame.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MAUI.Models;
+
+public class Videogame
+{
+    [Key]
+    public int id{ get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Price { get; set; }
+    public string Company { get; set; }
+    public string Gender { get; set; }
+}
